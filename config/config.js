591e7bfc746d26c6aecbbb9760292101d511b5f8
Dave Channon <withdave@users.noreply.github.com>
2024-05-01 09:10:59 +0100

    update config

--- config/config.js
+++ config/config.js
@@ -6,21 +6,22 @@
 
 // This is the hard-coded config for the M2M steps in impersonation, exposed below by getParameters().
 const myConfig = {
-    tenantHostname: "your-tenant.us.qlikcloud.com",
-    oAuthClientId: "5058cc83b4b6fae428e2c90e123",
-    oAuthClientSecret: "15932095c804414fd7a3b882fa2bf874bf126f19ae39d831234",
-    appId: "b0702f9b-46a3-46ff-8c8e-3a5f83a9b898"
+  tenantHostname: "your-tenant.us.qlikcloud.com", // format: "your-tenant.us.qlikcloud.com" (no protocol or trailing slashes)
+  oAuthClientId: "5058cc83b4b6fae428e2c90e77123",
+  oAuthClientSecret:
+    "15932095c804414fd7a3b882fa2bf874bf126f19ae39d83deb123",
+  appId: "b0702f9b-46a3-46ff-8c8e-3a5f83a9b898",
 };
 
 // Create a cut-down config without the client secret for use in the qlik-embed HEAD tag
 const myParamsConfig = {
-    tenantHostname: myConfig.tenantHostname,
-    oAuthClientId: myConfig.oAuthClientId,
-    appId: myConfig.appId
-}
+  tenantHostname: myConfig.tenantHostname,
+  oAuthClientId: myConfig.oAuthClientId,
+  appId: myConfig.appId,
+};
 
 const getParameters = async function (email) {
-    /*
+  /*
     This function should:
     - Accept a user or customer identifier (such as email)
     - Look up the correct Qlik Cloud tenant for that customer
@@ -33,9 +34,7 @@ const getParameters = async function (email) {
     embedding and impersonation. Do not do this in production.
 
     */
-    return myParamsConfig;
-  };
-  
-  
-  
-  export { myConfig,  getParameters};
+  return myParamsConfig;
+};
+
+export { myConfig, getParameters };
